a75143b69453fb5d47dc847ccbb31a56699cae7d
Lakusqo <lakuskuskus@gmail.com>
2023-02-10 11:30:33 +0100

    Final app

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -10,18 +10,21 @@ export class HousingListComponent implements OnInit {
 
   @Input() locationList: HousingLocation[] = [];
   results: HousingLocation[] = [];
-
   @Output() locationSelectedEvent = new EventEmitter<HousingLocation>();
 
-  constructor() { }
-
   ngOnInit(): void {
+
   }
 
   searchHousingLocations(searchText: string) {
-    if (!searchText) return;
+    //if (!searchText) return;
 
-    this.results = this.locationList.filter((location: any) => location.city.toLowerCase().includes(searchText.toLowerCase()));
+    this.results = this.locationList.filter(
+      (location: HousingLocation) => location.city
+        .toLowerCase()
+        .includes(
+            searchText.toLowerCase()
+      ));
   }
 
   selectHousingLocation(location: HousingLocation) {
